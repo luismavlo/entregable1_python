f1eb12fb3c63753e1e0e4f12479b0a4a061df863
Alexandra García <91643736+AlexandraC0DE@users.noreply.github.com>
2022-07-10 21:21:35 -0500

    add  comments

--- student.py
+++ student.py
@@ -4,22 +4,22 @@ class Student():
         self.list_student = list_student
 
     def find_winners(self, positions: int):
-           """
+    """
     La función find_winners hace un llamado a los 3 primeros lugares en el score.
     ordenado de mayor a menor por el index.
-
+    
     """
         return self.list_student[0:positions]
 
     def find_users_by_percent(self, percent: int):
-           """
+    """
     La función find_users_by_percent hace una consulta del score 
     y el acuracy a la lista de estudiantes
     For:
     Si el student['acuracy'] es mayor o igual al porcentaje 
     se aplica método append
     devolviendo una nueva lista en el main.py 
-
+    
     """
         student_filter_by_percent = []
         for student in self.list_student:
